4725ab4caaee0e51994a5a28c63b0c1a4d34e8cf
chickie8x <chickie8x.qn@gmail.com>
2024-04-09 20:01:47 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <span class="text-indigo-500 text-xl font-medium">Hello</span>
+  </div>
+</template>
+
+<script setup>
+
+</script>
